c082e41ea8c422b8e01fe1e921bf487ef96e55b2
cabaDev <carlosrbaezp@gmail.com>
2024-06-25 13:53:56 +0200

    all

---
+++ cabasTes.js
@@ -0,0 +1,18 @@
+function camisetaMujer ({talla = M,color, tela}) {
+
+}
+
+function camisetaMujer ({talla = "XL", color = "pink", tela ="algodon"}){
+
+}
+
+const camisetaMujer = {
+talla: "XL",
+Color: "pink",
+tela: "algodon",
+
+}
+
+const map1 = new Map();
+
+
