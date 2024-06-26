77bfe045c49fc892774a6bd9a655181ced015300
cabaDev <carlosrbaezp@gmail.com>
2024-06-26 11:53:39 +0200

    cambios

--- numerosPrimos.js
+++ numerosPrimos.js
@@ -1,23 +1,10 @@
-const array = numeros.from({ length: 99 }),
 
-function esPrimo(num) {
-    if (num <= 1)
-        return false;
+
+let array = [];
+
+for ( i = 0, <= i 50 +== ){
+
+    array.push(i)
 }
+console.log(array);
 
-if (num === 2) {
-    return true;
-}
-
-if (nume % 2 === 0) {
-    return false;
-}
-
-let divisible = Math.sqrt(num);
-for (let i =3; i <=divisible; i += 2){
-    if (num % i === 0){
-        return false;
-    }
-}
-
-console.log(esPrimo);
