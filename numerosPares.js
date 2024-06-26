a6df7c56d61e965b07d6736a3071f4f31ce1d8f6
cabaDev <carlosrbaezp@gmail.com>
2024-06-26 16:04:02 +0200

    numeros primos

--- numerosPares.js
+++ numerosPares.js
@@ -28,7 +28,7 @@ const isNumberPairPositive = (myNumero) => {
 
 const getPrimeNumber = (cantidadDeNumerosPares) => {
     let primos = [];
-    for (let index = 0; primos.length < cantidadDeNumerosPares; index++) {
+    for (let index = 2; primos.length < cantidadDeNumerosPares; index++) {
         console.log(index)
         if (isPrime(index)){
             console.log("add Prime number in array" , index)
@@ -38,10 +38,22 @@ const getPrimeNumber = (cantidadDeNumerosPares) => {
     console.log(primos)
     return primos;
 }
-
 const isPrime = (myNumero) => {
-    return myNumero > 0 && myNumero % 2 === 1
+    console.log("--- " , myNumero)
+    for (let currentNumber = 2; currentNumber < myNumero ; currentNumber++) {
+        console.log("Iteracion ", currentNumber -1 , " - current number", currentNumber)
+        if (myNumero % currentNumber === 0) {
+            console.log("No cumple - Iteracion ", currentNumber -1 , " - current number", currentNumber)
+            return false
+        }
+    }
+
+    return true
 }
 
 
-getPrimeNumber(10)
+
+let resultado = isPrime(1003)
+console.log("Es primo -> ", resultado)
+//let all = getPrimeNumber(5)
+//console.log(5)
