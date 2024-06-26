39b41d1254c184db08cfd42252dafb0d27ee6eec
cabaDev <carlosrbaezp@gmail.com>
2024-06-26 15:09:29 +0200

    numeros primos y pares

--- numerosPrimos.js
+++ numerosPrimos.js
@@ -1,23 +1,39 @@
-const array = numeros.from({ length: 99 }),
 
-function esPrimo(num) {
-    if (num <= 1)
-        return false;
+// creo el array
+let numbersZeroToHundred = [];
+
+for (let i = 0; i <= 99; i++) {
+    numbersZeroToHundred.push(i);
 }
 
-if (num === 2) {
-    return true;
-}
+let numbersImParZeroToHundred = array.filter(num => num % 2 !== 0);
 
-if (nume % 2 === 0) {
-    return false;
-}
+//console.log(arrayNoPar);
 
-let divisible = Math.sqrt(num);
-for (let i =3; i <=divisible; i += 2){
+// busco numeros primos 
+
+//elimino el numero 1
+let num = arrayNoPar;
+
+// divido los numeros del array para saber si son primos 
+// console.log(arrayNoPar)
+
+console.log(num)
+console.log(Math.sqrt(16))
+console.log(Math.sqrt(num))
+console.log(2 <= NaN)
+/**
+ * un número primo es un número natural mayor que 1 que tiene únicamente dos divisores positivos distintos: él mismo y el 1.1​
+ */
+for (let i = 2; i <= NaN; i++){
+    console.log("entra aqui alguna vez")
     if (num % i === 0){
-        return false;
+        return false
     }
-}
+};
 
-console.log(esPrimo);
+// imprimo los 10 primeros numeros primos
+let primeros10 = arrayNoPar.slice(0,10);
+//console.log(primeros10);
+
+
